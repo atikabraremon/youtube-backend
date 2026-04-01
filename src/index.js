06fb33b0bc6fb58db7d3172cd8dcf3bb4103b1d6
Atik Abrar Emon <atikabraremon@gmail.comm>
2026-04-01 16:36:27 +0600

    Utils code added

--- src/index.js
+++ src/index.js
@@ -8,28 +8,3 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const PORT = process.env.PORT || 8000;
 
 connectDB();
-
-console.log(process.env.MONGODB_URI);
-
-/* 
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERROR", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Error: ", error);
-    throw error;
-  }
-})();
-
-*/
